5daec7213d6c6290cdd9c07a9bfacdc9e03251c8
Naufal <u.naufaldaffa@gmail.com>
2026-07-05 14:05:03 +0700

    Revert ImageMagick check to original magick --version

--- converter.sh
+++ converter.sh
@@ -132,24 +132,13 @@ dependency_check "jq" "https://stedolan.github.io/jq/download/" "jq --version" "
 sponge () {
   cat > "${1}.tmp" && mv "${1}.tmp" "${1}"
 }
-if command -v magick &>/dev/null; then
-  convert () {
-    magick convert "$@"
-  }
-  mogrify () {
-    magick mogrify "$@"
-  }
-  IM_CMD="magick --version"
-else
-  convert () {
-    command convert "$@"
-  }
-  mogrify () {
-    command mogrify "$@"
-  }
-  IM_CMD="convert -version"
-fi
-dependency_check "imagemagick" "https://imagemagick.org/script/download.php" "$IM_CMD" ""
+convert () {
+  magick convert "$@"
+}
+mogrify () {
+  magick mogrify "$@"
+}
+dependency_check "imagemagick" "https://imagemagick.org/script/download.php" "magick --version" ""
 dependency_check "spritesheet-js" "https://www.npmjs.com/package/spritesheet-js" "-v spritesheet-js" ""
 status_message completion "All dependencies have been satisfied\n"
 
